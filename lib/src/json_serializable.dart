0b5720130d2e8e943015714524d361be8a62bf27
Kevin Moore <kevmoo@google.com>
2017-07-22 20:47:31 -0700

    More docs on JsonKey

--- lib/src/json_serializable.dart
+++ lib/src/json_serializable.dart
@@ -12,12 +12,24 @@ class JsonSerializable {
 }
 
 /// Customizes the how an annotated field is serialized
-///
-/// [name] allows the key in a JSON to have a value different than the field
-/// name.
 class JsonKey {
+  /// The key in a JSON map to use when reading and writing values corresponding
+  /// to the annotated fields.
+  ///
+  /// If `null`, the field name is used.
   final String name;
+
+  /// [true] if the generator should validate all values for `null` in
+  /// serialization code.
+  ///
+  /// Setting to [false] eliminates `null` verification in generated code, but
+  /// does not prevent `null` values from being created. Annotated classes
+  /// must implement their own `null` validation.
   final bool nullable;
+
+  /// Creates a new [JsonKey].
+  ///
+  /// Only required when the default behavior is not desired.
   const JsonKey({this.name, bool nullable: true})
       : this.nullable = nullable ?? true;
 }
