a00e79f470b0977dff5d044f8333e48ef5877fc3
Kevin Moore <kevmoo@users.noreply.github.com>
2017-08-15 13:37:06 -0700

    Trying to support the latest version of pkg/build (#31)

--- tool/build_actions.dart
+++ tool/build_actions.dart
@@ -6,10 +6,12 @@ import 'package:build_runner/build_runner.dart';
 import 'package:json_serializable/generators.dart';
 import 'package:source_gen/source_gen.dart';
 
-final PhaseGroup phases = new PhaseGroup.singleAction(
-    new PartBuilder(const [
-      const JsonSerializableGenerator(),
-      const JsonLiteralGenerator()
-    ]),
-    new InputSet('json_serializable',
-        const ['example/*.dart', 'test/test_files/*.dart']));
+final List<BuildAction> buildActions = [
+  new BuildAction(
+      new PartBuilder(const [
+        const JsonSerializableGenerator(),
+        const JsonLiteralGenerator()
+      ]),
+      'json_serializable',
+      inputs: const ['example/*.dart', 'test/test_files/*.dart'])
+];
