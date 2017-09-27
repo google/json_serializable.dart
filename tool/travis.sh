3e62422bf5f4110835880b0b369624bc3bf2a597
Kevin Moore <kevmoo@google.com>
2017-09-27 10:31:55 -0700

    Update travis with mono_repo magic

---
+++ tool/travis.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Created with https://github.com/dart-lang/mono_repo
+
+# Fast fail the script on failures.
+set -e
+
+if [ -z "$PKG" ]; then
+  echo -e "[31mPKG environment variable must be set![0m"
+  exit 1
+elif [ -z "$TASK" ]; then
+  echo -e "[31mTASK environment variable must be set![0m"
+  exit 1
+fi
+
+pushd $PKG
+pub upgrade
+
+case $TASK in
+dartanalyzer) echo
+  echo -e "[1mTASK: dartanalyzer[22m"
+  dartanalyzer .
+  ;;
+dartfmt) echo
+  echo -e "[1mTASK: dartfmt[22m"
+  dartfmt -n --set-exit-if-changed .
+  ;;
+test) echo
+  echo -e "[1mTASK: test[22m"
+  pub run test --run-skipped
+  ;;
+*) echo -e "[31mNot expecting TASK '${TASK}'. Error![0m"
+  exit 1
+  ;;
+esac
