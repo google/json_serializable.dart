2d61ba1c81a8c801ec6b15c1f42abc78cd05aaa6
Kevin Moore <kevmoo@users.noreply.github.com>
2017-10-09 11:32:04 -0700

    source_gen custom header – for copyright (#59)

--- json_serializable/tool/build_actions.dart
+++ json_serializable/tool/build_actions.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2017, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -8,10 +8,18 @@ import 'package:source_gen/source_gen.dart';
 
 final List<BuildAction> buildActions = [
   new BuildAction(
-      new PartBuilder(const [
-        const JsonSerializableGenerator(),
-        const JsonLiteralGenerator()
-      ]),
-      'json_serializable',
-      inputs: const ['example/*.dart', 'test/test_files/*.dart'])
+    new PartBuilder(
+        const [const JsonSerializableGenerator(), const JsonLiteralGenerator()],
+        header: _copyrightHeader),
+    'json_serializable',
+    inputs: const ['example/*.dart', 'test/test_files/*.dart'],
+  )
 ];
+
+final _copyrightHeader =
+    '''// Copyright (c) 2017, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+$defaultFileHeader
+''';
