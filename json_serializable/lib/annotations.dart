d37fe8a1d90fdccd65a018e8e8a7ec18ad59e55d
Kevin Moore <kevmoo@google.com>
2017-11-03 16:17:57 -0700

    Remove deprecated `annotations.dart` library

--- json_serializable/lib/annotations.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-@Deprecated('Use `package:json_annotation` instead.')
-library json_serializable.annotations;
-
-export 'package:json_annotation/json_annotation.dart';
