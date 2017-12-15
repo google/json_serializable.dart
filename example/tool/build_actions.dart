2c40819984b6da5059e0e10def009e361b714c94
Kevin Moore <kevmoo@google.com>
2017-12-14 21:28:17 -0800

    example: improve build_action docs and cleanup example_test.dart

--- example/tool/build_actions.dart
+++ example/tool/build_actions.dart
@@ -2,17 +2,17 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Defines the `BuildAction` class.
+/// Defines [BuildAction]
 import 'package:build_runner/build_runner.dart';
 
-// Defines `JsonSerializableGenerator` and `JsonLiteralGenerator`
+/// Defines [JsonSerializableGenerator] and [JsonLiteralGenerator]
 import 'package:json_serializable/generators.dart';
 
-// Defines `PartBuilder`
+/// Defines [PartBuilder]
 import 'package:source_gen/source_gen.dart';
 
-// The list of `BuildAction`s  used by `build.dart` and `watch.dart` to
-// generate source code for this package.
+/// The list of [BuildAction]s  used by `build.dart` and `watch.dart` to
+/// generate source code for this package.
 List<BuildAction> get buildActions => [
       new BuildAction(
         // `PartBuilder` is a `Builder` defined by `package:source_gen` for
@@ -38,6 +38,8 @@ List<BuildAction> get buildActions => [
       ),
     ];
 
+/// Used in the `header` argument to the [PartBuilder] constructor to add the
+/// standard copyright notice to all generated files.
 final _copyrightContent =
     '''// Copyright (c) 2017, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
