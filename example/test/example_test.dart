0b3fe4a8edcc65b89b9849de585f8adcf26826f5
Kevin Moore <kevmoo@google.com>
2017-12-14 21:28:17 -0800

    example: improve build_action docs and cleanup example_test.dart

--- example/test/example_test.dart
+++ example/test/example_test.dart
@@ -11,18 +11,22 @@ void main() {
   test('JsonSerializable', () {
     final person = new Person('Inigo', 'Montoya', new DateTime(1560, 5, 5));
 
-    final prettyJson = const JsonEncoder.withIndent(' ').convert(person);
-    print(prettyJson);
+    final personJson = _encode(person);
 
     final person2 =
-        new Person.fromJson(JSON.decode(prettyJson) as Map<String, dynamic>);
+        new Person.fromJson(JSON.decode(personJson) as Map<String, dynamic>);
 
     expect(person.firstName, person2.firstName);
     expect(person.lastName, person2.lastName);
     expect(person.dateOfBirth, person2.dateOfBirth);
+
+    expect(_encode(person2), equals(personJson));
   });
 
   test('JsonLiteral', () {
     expect(glossaryData, hasLength(1));
   });
 }
+
+String _encode(Object object) =>
+    const JsonEncoder.withIndent(' ').convert(object);
