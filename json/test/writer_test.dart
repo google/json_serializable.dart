9121caf69245162ef1b5ff6042dbe7ac217237c7
Kevin Moore <kevmoo@google.com>
2017-12-17 23:22:16 -0800

    test cleanup

--- json/test/writer_test.dart
+++ json/test/writer_test.dart
@@ -86,20 +86,22 @@ main() {
   };
 
   for (var withBytes in [false, true]) {
+    Converter wrap(Converter<Object, String> source) {
+      if (withBytes) {
+        return source.fuse(const Utf8Encoder());
+      }
+      return source;
+    }
+
     group(withBytes ? 'JsonUtf8Encoder' : 'JsonEncoder', () {
       for (var pretty in [false, true]) {
-        group(pretty ? 'pretty' : 'flat', () {
-          final indent = pretty ? ' ' : null;
-          Converter sdkToJson = new sdk.JsonEncoder.withIndent(indent);
-          Converter encoderToJson = new JsonEncoder(indent: indent);
-          Converter encoderWriter =
-              new JsonEncoder(indent: indent, writer: _writer);
+        final indent = pretty ? ' ' : null;
 
-          if (withBytes) {
-            sdkToJson = sdkToJson.fuse(const Utf8Encoder());
-            encoderToJson = encoderToJson.fuse(const Utf8Encoder());
-            encoderWriter = encoderWriter.fuse(const Utf8Encoder());
-          }
+        group(pretty ? 'pretty' : 'flat', () {
+          final sdkToJson = wrap(new sdk.JsonEncoder.withIndent(indent));
+          final encoderToJson = wrap(new JsonEncoder(indent: indent));
+          final encoderWriter =
+              wrap(new JsonEncoder(indent: indent, writer: _writer));
 
           _items.forEach((k, v) {
             test(k, () {
