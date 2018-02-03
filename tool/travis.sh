7cf8d4905f7a53870bb9c5f1bd18d88b437ab49c
Kevin Moore <kevmoo@google.com>
2018-02-02 16:41:13 -0800

    Don't run tests against json_serializable on stable

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,10 +5,10 @@
 set -e
 
 if [ -z "$PKG" ]; then
-  echo -e "[31mPKG environment variable must be set![0m"
+  echo -e '\033[31mPKG environment variable must be set!\033[0m'
   exit 1
 elif [ -z "$TASK" ]; then
-  echo -e "[31mTASK environment variable must be set![0m"
+  echo -e '\033[31mTASK environment variable must be set!\033[0m'
   exit 1
 fi
 
@@ -16,19 +16,27 @@ pushd $PKG
 pub upgrade
 
 case $TASK in
-dartanalyzer) echo
-  echo -e "[1mTASK: dartanalyzer[22m"
+dartanalyzer_0) echo
+  echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
+  echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+  dartanalyzer --fatal-infos --fatal-warnings .
+  ;;
+dartanalyzer_1) echo
+  echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
+  echo -e 'dartanalyzer .'
   dartanalyzer .
   ;;
 dartfmt) echo
-  echo -e "[1mTASK: dartfmt[22m"
+  echo -e '\033[1mTASK: dartfmt\033[22m'
+  echo -e 'dartfmt -n --set-exit-if-changed .'
   dartfmt -n --set-exit-if-changed .
   ;;
 test) echo
-  echo -e "[1mTASK: test[22m"
+  echo -e '\033[1mTASK: test\033[22m'
+  echo -e 'pub run test --run-skipped'
   pub run test --run-skipped
   ;;
-*) echo -e "[31mNot expecting TASK '${TASK}'. Error![0m"
+*) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
   exit 1
   ;;
 esac
