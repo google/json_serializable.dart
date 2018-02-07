423377df7c97ed27fc1a73d660ccd5215cd4d8cb
Kevin Moore <kevmoo@users.noreply.github.com>
2018-02-07 10:55:46 -0800

    use latest mono_repo (#99)

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,36 +7,38 @@ set -e
 if [ -z "$PKG" ]; then
   echo -e '\033[31mPKG environment variable must be set!\033[0m'
   exit 1
-elif [ -z "$TASK" ]; then
-  echo -e '\033[31mTASK environment variable must be set!\033[0m'
+fi
+
+if [ "$#" == "0" ]; then
+  echo -e '\033[31mAt least one task argument must be provided!\033[0m'
   exit 1
 fi
 
 pushd $PKG
 pub upgrade
 
-case $TASK in
-dartanalyzer_0) echo
-  echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
-  echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
-  dartanalyzer --fatal-infos --fatal-warnings .
-  ;;
-dartanalyzer_1) echo
-  echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
-  echo -e 'dartanalyzer .'
-  dartanalyzer .
-  ;;
-dartfmt) echo
-  echo -e '\033[1mTASK: dartfmt\033[22m'
-  echo -e 'dartfmt -n --set-exit-if-changed .'
-  dartfmt -n --set-exit-if-changed .
-  ;;
-test) echo
-  echo -e '\033[1mTASK: test\033[22m'
-  echo -e 'pub run test --run-skipped'
-  pub run test --run-skipped
-  ;;
-*) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-  exit 1
-  ;;
-esac
+while (( "$#" )); do
+  TASK=$1
+  case $TASK in
+  dartanalyzer) echo
+    echo -e '\033[1mTASK: dartanalyzer\033[22m'
+    echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
+    dartanalyzer --fatal-infos --fatal-warnings .
+    ;;
+  dartfmt) echo
+    echo -e '\033[1mTASK: dartfmt\033[22m'
+    echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt -n --set-exit-if-changed .
+    ;;
+  test) echo
+    echo -e '\033[1mTASK: test\033[22m'
+    echo -e 'pub run test --run-skipped'
+    pub run test --run-skipped
+    ;;
+  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    exit 1
+    ;;
+  esac
+
+  shift
+done
