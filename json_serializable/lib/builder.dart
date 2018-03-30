3eabbf23f231a18f631f2294158fabf94d4bc292
Kevin Moore <kevmoo@google.com>
2018-03-30 10:28:30 -0700

    Document builder.dart as not interesting for most end-users

--- json_serializable/lib/builder.dart
+++ json_serializable/lib/builder.dart
@@ -2,6 +2,16 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// Configuration for using `package:build`-compatible build systems.
+///
+/// See:
+/// * [build_runner](https://pub.dartlang.org/packages/build_runner)
+///
+/// This library is **not** intended to be imported by typical end-users unless
+/// you are creating a custom compilation pipeline. See documentation for
+/// details, and `build.yaml` for how these builders are configured by default.
+library json_serializable.builder;
+
 import 'package:build/build.dart';
 import 'package:logging/logging.dart';
 
