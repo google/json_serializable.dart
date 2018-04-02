5f3977dfa6d9669e47fb865d1f34964b06a3729a
Kevin Moore <kevmoo@users.noreply.github.com>
2018-04-02 11:19:43 -0700

    Centralize and improve String literal handling (#125)
    * Share code for escape String literals
    * Add support for escaping strings w/ new line characters

--- json_serializable/lib/src/utils.dart
+++ json_serializable/lib/src/utils.dart
@@ -11,6 +11,43 @@ import 'package:analyzer/src/dart/resolver/inheritance_manager.dart'
 
 import 'package:source_gen/source_gen.dart';
 
+/// Returns a quoted String literal for [value] that can be used in generated
+/// Dart code.
+// TODO: still need handle triple singe/double quotes within `value`
+String escapeDartString(String value) {
+  if (value.contains('\n')) {
+    return "r'''\n$value'''";
+  }
+
+  var containsDollar = value.contains(r'$');
+
+  if (value.contains("'")) {
+    if (value.contains('"')) {
+      // `value` contains both single and double quotes.
+      // The only safe way to wrap the content is to escape all of the
+      // problematic characters.
+      var string = value
+          .replaceAll(r'$', r'\$')
+          .replaceAll('"', r'\"')
+          .replaceAll("'", r"\'");
+      return "'$string'";
+    } else if (containsDollar) {
+      // `value` contains "'" and "$", but not '"'.
+      // Safely wrap it in a raw string within double-quotes.
+      return 'r"$value"';
+    }
+    return '"$value"';
+  } else if (containsDollar) {
+    // `value` contains "$", but no "'"
+    // wrap it in a raw string using single quotes
+    return "r'$value'";
+  }
+
+  // `value` contains no problematic characters - except for '"' maybe.
+  // Wrap it in standard single-quotes.
+  return "'$value'";
+}
+
 String commonNullPrefix(
         bool nullable, String expression, String unsafeExpression) =>
     nullable
