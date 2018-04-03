ab877bf34093286ae2b20bb2df996e7ff37efd39
Kevin Moore <kevmoo@google.com>
2018-04-03 12:11:14 -0700

    Prepare to release v0.2.3 json_annotations
    Added a lot of doc comments

--- json_annotation/lib/json_annotation.dart
+++ json_annotation/lib/json_annotation.dart
@@ -2,7 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Use these annotations on members you'd like to enable source generation on.
+/// Provides annotation classes to use with
+/// [json_serializable](https://pub.dartlang.org/packages/json_serializable).
+///
+/// Also contains helper functions and classes – prefixed with `$` used by
+/// `json_serializable` when generating wrappers.
+library json_annotation;
 
 export 'src/json_literal.dart';
 export 'src/json_serializable.dart';
--- json_annotation/lib/src/json_literal.dart
+++ json_annotation/lib/src/json_literal.dart
@@ -2,6 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// An annotation used to generate a private field containing the contents of a
+/// JSON file.
+///
+/// The annotation can be applied to any member, but usually it's applied to
+/// getter.
 class JsonLiteral {
   /// The relative path from the Dart file with the annotation to the file
   /// containing the source JSON.
