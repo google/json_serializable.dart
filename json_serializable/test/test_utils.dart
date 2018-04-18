e6df15d2b0b35c830ab73bb8315fb530313b9715
Kevin Moore <kevmoo@google.com>
2018-04-18 09:08:57 -0700

    Remove many unused utils in tests

--- json_serializable/test/test_utils.dart
+++ json_serializable/test/test_utils.dart
@@ -5,9 +5,6 @@
 import 'dart:convert';
 import 'dart:mirrors';
 
-import 'package:analyzer/dart/ast/ast.dart';
-import 'package:analyzer/dart/ast/standard_resolution_map.dart';
-import 'package:analyzer/dart/element/element.dart';
 import 'package:path/path.dart' as p;
 import 'package:source_gen/source_gen.dart';
 import 'package:test/test.dart';
@@ -47,32 +44,6 @@ class FeatureMatcher<T> extends CustomMatcher {
 
   @override
   featureValueOf(covariant T actual) => _feature(actual);
-}
-
-/// Returns all of the declarations in [unit], including [unit] as the first
-/// item.
-Iterable<Element> getElementsFromLibraryElement(LibraryElement unit) sync* {
-  yield unit;
-  for (var cu in unit.units) {
-    for (var compUnitMember in cu.unit.declarations) {
-      yield* _getElements(compUnitMember);
-    }
-  }
-}
-
-Iterable<Element> _getElements(CompilationUnitMember member) {
-  if (member is TopLevelVariableDeclaration) {
-    return member.variables.variables
-        .map(resolutionMap.elementDeclaredByVariableDeclaration);
-  }
-  var element = resolutionMap.elementDeclaredByDeclaration(member);
-
-  if (element == null) {
-    print([member, member.runtimeType, member.element]);
-    throw new Exception('Could not find any elements for the provided unit.');
-  }
-
-  return [element];
 }
 
 void roundTripObject<T>(T object, T factory(Map<String, dynamic> json)) {
