048e13ce81a28239e287c1739019b6122b264cac
Kevin Moore <kevmoo@google.com>
2018-05-10 15:40:55 -0700

    json_annotation: put the wrapper helpers in their own file

--- json_annotation/lib/src/json_serializable.dart
+++ json_annotation/lib/src/json_serializable.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:collection';
-
 /// An annotation used to specify a class to generate code for.
 class JsonSerializable {
   // TODO(kevmoo): document these fields
@@ -105,75 +103,4 @@ class JsonKey {
       this.ignore,
       this.fromJson,
       this.toJson});
-}
-
-/// Helper classes used in generated code when
-/// `JsonSerializableGenerator.useWrappers` is `true`.
-///
-/// Should not be used directly.
-abstract class $JsonMapWrapper extends UnmodifiableMapBase<String, dynamic> {}
-
-/// Helper function used in generated code when
-/// `JsonSerializableGenerator.useWrappers` is `true`.
-///
-/// Should not be used directly.
-Map<String, dynamic> $wrapMap<K, V>(
-        Map<K, V> source, dynamic converter(V key)) =>
-    new _MappingMap(source, converter);
-
-/// Helper function used in generated code when
-/// `JsonSerializableGenerator.useWrappers` is `true`.
-///
-/// Should not be used directly.
-Map<String, dynamic> $wrapMapHandleNull<K, V>(
-        Map<K, V> source, dynamic converter(V key)) =>
-    source == null ? null : new _MappingMap(source, converter);
-
-/// Helper function used in generated code when
-/// `JsonSerializableGenerator.useWrappers` is `true`.
-///
-/// Should not be used directly.
-List<dynamic> $wrapList<T>(List<T> source, dynamic converter(T key)) =>
-    new _MappingList(source, converter);
-
-/// Helper function used in generated code when
-/// `JsonSerializableGenerator.useWrappers` is `true`.
-///
-/// Should not be used directly.
-List<dynamic> $wrapListHandleNull<T>(
-        List<T> source, dynamic converter(T key)) =>
-    source == null ? null : new _MappingList(source, converter);
-
-typedef dynamic _Convert<S>(S value);
-
-class _MappingList<S> extends ListBase<dynamic> {
-  final List<S> _source;
-  final _Convert<S> _converter;
-
-  _MappingList(this._source, this._converter);
-
-  @override
-  dynamic operator [](int index) => _converter(_source[index]);
-
-  @override
-  operator []=(int index, dynamic value) => throw new UnsupportedError('');
-
-  @override
-  int get length => _source.length;
-
-  @override
-  set length(int value) => throw new UnsupportedError('');
-}
-
-class _MappingMap<K, V> extends UnmodifiableMapBase<String, dynamic> {
-  final Map<K, V> _source;
-  final _Convert<V> _converter;
-
-  _MappingMap(this._source, this._converter);
-
-  @override
-  Iterable<String> get keys => _source.keys.map((k) => k as String);
-
-  @override
-  dynamic operator [](Object key) => _converter(_source[key]);
 }
