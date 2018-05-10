2b8fb585c9b0d3535ceb17529fa2ba25d08b9b3a
Kevin Moore <kevmoo@google.com>
2018-05-10 08:20:39 -0700

    Add missing dep on pkg:meta

--- json_serializable/lib/src/json_key_helpers.dart
+++ json_serializable/lib/src/json_key_helpers.dart
@@ -8,7 +8,7 @@ import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/dart/constant/value.dart';
 
 import 'package:json_annotation/json_annotation.dart';
-import 'package:meta/meta.dart';
+import 'package:meta/meta.dart' show alwaysThrows;
 import 'package:source_gen/source_gen.dart';
 
 @alwaysThrows
