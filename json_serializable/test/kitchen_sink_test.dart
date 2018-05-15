96f9ae192920f3f4824b1111ecf86cc7d09e847f
Kevin Moore <kevmoo@google.com>
2018-05-15 14:03:25 -0700

    kitchen sink test: Make sure test values cover all keys

--- json_serializable/test/kitchen_sink_test.dart
+++ json_serializable/test/kitchen_sink_test.dart
@@ -22,6 +22,14 @@ final _isACastError = const isInstanceOf<CastError>();
 final _isATypeError = const isInstanceOf<TypeError>();
 
 void main() {
+  test('valid values covers all keys', () {
+    expect(_validValues.keys, orderedEquals(_expectedOrder));
+  });
+
+  test('inlavid values covers all keys', () {
+    expect(_invalidValues.keys, orderedEquals(_expectedOrder));
+  });
+
   group('nullable', () {
     group('unwrapped', () {
       _nullableTests(nullable.testFactory, nullable.testFromJson);
@@ -170,56 +178,14 @@ void _sharedTests(
     expect(json.keys, orderedEquals(_expectedOrder));
   });
 
+  test('valid values round-trip', () {
+    expect(loudEncode(_validValues), loudEncode(fromJson(_validValues)));
+  });
+
   group('a bad value for', () {
-    final input = const {
-      'no-42': 0,
-      'dateTime': '2018-05-10T14:20:58.927',
-      'iterable': const [],
-      'dynamicIterable': const [],
-      'objectIterable': const [],
-      'intIterable': const [],
-      'datetime-iterable': const [],
-      'list': const [],
-      'dynamicList': const [],
-      'objectList': const [],
-      'intList': const [],
-      'dateTimeList': const [],
-      'map': const <String, dynamic>{},
-      'stringStringMap': const {},
-      'stringIntMap': const {},
-      'stringDateTimeMap': const <String, dynamic>{},
-      'crazyComplex': const [],
-      'val': const {},
-      'writeNotNull': null,
-      r'$string': null
-    };
-
-    test('nothing succeeds', () {
-      expect(loudEncode(input), loudEncode(fromJson(input)));
-    });
-
-    for (var e in {
-      'no-42': 42,
-      'dateTime': true,
-      'iterable': true,
-      'dynamicIterable': true,
-      'intIterable': [true],
-      'datetime-iterable': [true],
-      'list': true,
-      'dynamicList': true,
-      'objectList': true,
-      'intList': [true],
-      'dateTimeList': [true],
-      'stringStringMap': {'key': 42},
-      'stringIntMap': {'key': 'value'},
-      'stringDateTimeMap': {'key': 42},
-      'crazyComplex': [true],
-      'val': {'key': 42},
-      'writeNotNull': 42,
-      r'$string': true,
-    }.entries) {
+    for (var e in _invalidValues.entries) {
       test('`${e.key}` fails', () {
-        var copy = new Map<String, dynamic>.from(input);
+        var copy = new Map<String, dynamic>.from(_validValues);
         copy[e.key] = e.value;
         expect(() => fromJson(copy),
             throwsA(anyOf(_isATypeError, _isACastError, isArgumentError)));
@@ -227,6 +193,52 @@ void _sharedTests(
     }
   });
 }
+
+final _validValues = const {
+  'no-42': 0,
+  'dateTime': '2018-05-10T14:20:58.927',
+  'iterable': const [],
+  'dynamicIterable': const [],
+  'objectIterable': const [],
+  'intIterable': const [],
+  'datetime-iterable': const [],
+  'list': const [],
+  'dynamicList': const [],
+  'objectList': const [],
+  'intList': const [],
+  'dateTimeList': const [],
+  'map': const <String, dynamic>{},
+  'stringStringMap': const {},
+  'stringIntMap': const {},
+  'stringDateTimeMap': const <String, dynamic>{},
+  'crazyComplex': const [],
+  'val': const {},
+  'writeNotNull': null,
+  r'$string': null
+};
+
+final _invalidValues = const {
+  'no-42': 42,
+  'dateTime': true,
+  'iterable': true,
+  'dynamicIterable': true,
+  'objectIterable': true,
+  'intIterable': const [true],
+  'datetime-iterable': const [true],
+  'list': true,
+  'dynamicList': true,
+  'objectList': true,
+  'intList': const [true],
+  'dateTimeList': const [true],
+  'map': true,
+  'stringStringMap': const {'key': 42},
+  'stringIntMap': const {'key': 'value'},
+  'stringDateTimeMap': const {'key': 42},
+  'crazyComplex': const [true],
+  'val': const {'key': 42},
+  'writeNotNull': 42,
+  r'$string': true,
+};
 
 final _excludeIfNullKeys = [
   'dateTime',
