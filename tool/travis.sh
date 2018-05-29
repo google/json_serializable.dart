8de9b5912682cdf12fe43ffff1a6b28af86f4d20
Kevin Moore <kevmoo@google.com>
2018-05-29 12:30:08 -0700

    Try firefox?

--- tool/travis.sh
+++ tool/travis.sh
@@ -21,8 +21,8 @@ while (( "$#" )); do
   case $TASK in
   command) echo
     echo -e '\033[1mTASK: command\033[22m'
-    echo -e 'pub run build_runner test -- -p chrome'
-    pub run build_runner test -- -p chrome || EXIT_CODE=$?
+    echo -e 'pub run build_runner test -- -p firefox'
+    pub run build_runner test -- -p firefox || EXIT_CODE=$?
     ;;
   dartanalyzer) echo
     echo -e '\033[1mTASK: dartanalyzer\033[22m'
