f1e1beb0fdec95639d64d6d66c8853eb672dead2
Kevin Moore <kevmoo@users.noreply.github.com>
2018-06-12 10:41:55 -0700

    nit

--- json_serializable/tool/build.dart
+++ json_serializable/tool/build.dart
@@ -3,7 +3,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:io' show exitCode;
 
 import 'package:build/build.dart';
@@ -119,6 +118,6 @@ final List<BuilderApplication> builders = [
       hideOutput: true),
 ];
 
-Future main(List<String> args) async {
+void main(List<String> args) async {
   exitCode = await run(args, builders);
 }
