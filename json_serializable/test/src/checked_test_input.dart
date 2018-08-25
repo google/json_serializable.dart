803841985de619d5b720de6c88aaf9fb880b149f
Kevin Moore <kevmoo@google.com>
2018-08-24 17:46:08 -0700

    fix: disallowUnrecognizedKeys should only include settable fields
    Before we'd include all properties, even if they weren't set in the
    factory
    
    Fixes https://github.com/dart-lang/json_serializable/issues/291

--- json_serializable/test/src/checked_test_input.dart
+++ json_serializable/test/src/checked_test_input.dart
@@ -1,24 +1,35 @@
 part of '_json_serializable_test_input.dart';
 
 @ShouldGenerate(r'''
-CheckedWithANonCtorGetter _$CheckedWithANonCtorGetterFromJson(
+WithANonCtorGetterChecked _$WithANonCtorGetterCheckedFromJson(
     Map<String, dynamic> json) {
-  return $checkedNew('CheckedWithANonCtorGetter', json, () {
-    $checkKeys(json, allowedKeys: ['items', 'legth']);
-    var val = CheckedWithANonCtorGetter($checkedConvert(
+  return $checkedNew('WithANonCtorGetterChecked', json, () {
+    $checkKeys(json, allowedKeys: ['items']);
+    var val = WithANonCtorGetterChecked($checkedConvert(
         json, 'items', (v) => (v as List)?.map((e) => e as String)?.toList()));
     return val;
   });
 }
-
-Map<String, dynamic> _$CheckedWithANonCtorGetterToJson(
-        CheckedWithANonCtorGetter instance) =>
-    <String, dynamic>{'items': instance.items};
 ''', checked: true)
-@JsonSerializable(disallowUnrecognizedKeys: true)
-class CheckedWithANonCtorGetter {
+@JsonSerializable(disallowUnrecognizedKeys: true, createToJson: false)
+class WithANonCtorGetterChecked {
   final List<String> items;
   int get legth => items.length;
 
-  CheckedWithANonCtorGetter(this.items);
+  WithANonCtorGetterChecked(this.items);
+}
+
+@ShouldGenerate(r'''
+WithANonCtorGetter _$WithANonCtorGetterFromJson(Map<String, dynamic> json) {
+  $checkKeys(json, allowedKeys: ['items']);
+  return WithANonCtorGetter(
+      (json['items'] as List)?.map((e) => e as String)?.toList());
+}
+''')
+@JsonSerializable(disallowUnrecognizedKeys: true, createToJson: false)
+class WithANonCtorGetter {
+  final List<String> items;
+  int get legth => items.length;
+
+  WithANonCtorGetter(this.items);
 }
