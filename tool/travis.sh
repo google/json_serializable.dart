4cc50039351bcaea83d85c3500d1b85c30d166b4
Kevin Moore <kevmoo@google.com>
2018-09-24 12:13:04 -0700

    Use latest mono_repo to support branch config

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with https://github.com/dart-lang/mono_repo
+# Created with package:mono_repo v1.2.0
 
 if [ -z "$PKG" ]; then
   echo -e '\033[31mPKG environment variable must be set!\033[0m'
