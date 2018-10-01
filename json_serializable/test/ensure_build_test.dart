c17509ad3c6211f86a5bdf8325480a3a2b471d6e
Kevin Moore <kevmoo@users.noreply.github.com>
2018-10-01 12:16:46 -0700

    Fix unnecessary const (#330)
    Only analyze with --fatal-infos in dev channel, due to bug

--- json_serializable/test/ensure_build_test.dart
+++ json_serializable/test/ensure_build_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-@Tags(const ['presubmit-only'])
+@Tags(['presubmit-only'])
 import 'package:build_verify/build_verify.dart';
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
