3c4a2523cc2788604bb8ebeb74e8164a2dde2639
Kevin Moore <kevmoo@google.com>
2018-10-19 10:10:53 -0700

    Update example pkg versions and small tweaks to the code

--- example/lib/example.g.dart
+++ example/lib/example.g.dart
@@ -38,8 +38,9 @@ Map<String, dynamic> _$PersonToJson(Person instance) {
 }
 
 Order _$OrderFromJson(Map<String, dynamic> json) {
-  return Order(
-      json['date'] == null ? null : _dateTimeFromEpochUs(json['date'] as int))
+  return Order(json['date'] == null
+      ? null
+      : Order._dateTimeFromEpochUs(json['date'] as int))
     ..count = json['count'] as int
     ..itemNumber = json['itemNumber'] as int
     ..isRushed = json['isRushed'] as bool
@@ -48,7 +49,7 @@ Order _$OrderFromJson(Map<String, dynamic> json) {
         : Item.fromJson(json['item'] as Map<String, dynamic>)
     ..prepTime = json['prep-time'] == null
         ? null
-        : _durationFromMillseconds(json['prep-time'] as int);
+        : Order._durationFromMilliseconds(json['prep-time'] as int);
 }
 
 Map<String, dynamic> _$OrderToJson(Order instance) {
@@ -68,9 +69,9 @@ Map<String, dynamic> _$OrderToJson(Order instance) {
       'prep-time',
       instance.prepTime == null
           ? null
-          : _durationToMilliseconds(instance.prepTime));
-  writeNotNull(
-      'date', instance.date == null ? null : _dateTimeToEpochUs(instance.date));
+          : Order._durationToMilliseconds(instance.prepTime));
+  writeNotNull('date',
+      instance.date == null ? null : Order._dateTimeToEpochUs(instance.date));
   return val;
 }
 
