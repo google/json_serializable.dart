a0092def31f0455b7bf374c7a0cf2ee7dabe230f
Kevin Moore <kevmoo@google.com>
2019-03-14 17:40:42 -0700

    Continued cleanup of kitchen_sink tests

---
+++ json_serializable/test/kitchen_sink/kitchen_sink.factories.dart
@@ -0,0 +1,17 @@
+import 'kitchen_sink.dart' as normal;
+import 'kitchen_sink.g_any_map.dart' as any_map;
+import 'kitchen_sink.g_any_map__checked__non_nullable.dart'
+    as any_map__checked__non_nullable;
+import 'kitchen_sink.g_any_map__non_nullable.dart' as any_map__non_nullable;
+import 'kitchen_sink.g_any_map__non_nullable__use_wrappers.dart'
+    as any_map__non_nullable__use_wrappers;
+import 'kitchen_sink.g_any_map__use_wrappers.dart' as any_map__use_wrappers;
+
+const factories = [
+  normal.factory,
+  any_map.factory,
+  any_map__checked__non_nullable.factory,
+  any_map__non_nullable.factory,
+  any_map__non_nullable__use_wrappers.factory,
+  any_map__use_wrappers.factory,
+];
