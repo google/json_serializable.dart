d60c0a51169ea82e8381a1f902710cfa079143f2
Kevin Moore <kevmoo@users.noreply.github.com>
2019-03-26 06:59:58 -0700

    Update doc generation (#427)
    - split out JsonKey and JsonSerializable
    - document the build.yaml key for JsonSerializable
    - include all of the content of the doc comments
    - wire up anchor links correctly

--- json_serializable/tool/doc_builder.dart
+++ json_serializable/tool/doc_builder.dart
@@ -3,56 +3,10 @@ import 'dart:convert';
 
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
+import 'package:json_serializable/src/utils.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder docBuilder([_]) => _DocBuilder();
-
-final _ref = RegExp('\\[([^\\]]+)\\]');
-
-class _FieldInfo implements Comparable<_FieldInfo> {
-  final FieldElement field;
-
-  ClassElement get owner => field.enclosingElement;
-
-  String get description {
-    final description = LineSplitter.split(field.documentationComment)
-        .map((line) {
-          if (line.startsWith('///')) {
-            line = line.substring(3).trim();
-          }
-          return line;
-        })
-        .takeWhile((line) => line.isNotEmpty)
-        .join(' ');
-
-    return description
-        .replaceAllMapped(_ref, (m) => '`${m[1]}`')
-        .replaceAll(' (the default)', '');
-  }
-
-  String get defaultValue => owner.constructors.single.parameters
-      .singleWhere((pe) => pe.name == field.name)
-      .defaultValueCode;
-
-  _FieldInfo(this.field);
-
-  @override
-  int compareTo(_FieldInfo other) => field.name.compareTo(other.field.name);
-
-  @override
-  String toString() => '_FieldThing($field)';
-}
-
-final _doubleTickEscape = RegExp('`([ &;,:\\\$_a-zA-Z]+)`');
-
-String _escapeDescription(String input) {
-  input = htmlEscape.convert(input);
-  input = input.replaceAllMapped(_doubleTickEscape, (e) {
-    return '<code>${e[1]}</code>';
-  });
-
-  return input;
-}
 
 class _DocBuilder extends Builder {
   @override
@@ -60,60 +14,39 @@ class _DocBuilder extends Builder {
     final lib = LibraryReader(await buildStep.resolver.libraryFor(
         AssetId.resolve('package:json_annotation/json_annotation.dart')));
 
-    final descriptions = <String, _FieldInfo>{};
-    final fieldMap = <String, List<_FieldInfo>>{};
+    final descriptionMap = <String, List<_FieldInfo>>{};
 
-    void processClass(ClassElement ce) {
-      for (var fe in ce.fields.where((fe) => !fe.isStatic)) {
-        final list = fieldMap.putIfAbsent(ce.name, () => <_FieldInfo>[]);
-        final newThing = descriptions[fe.name] = _FieldInfo(fe);
-        list.add(newThing);
-        list.sort();
+    for (var className in _annotationClasses) {
+      final descriptions = <_FieldInfo>[];
 
-        final existing = descriptions[fe.name];
-        if (existing != null && newThing.description != existing.description) {
-          log.severe([
-            'Duplicate field with different description',
-            fe.name,
-            existing.description,
-            newThing.description
-          ].join('\n'));
-        }
+      for (var fe
+          in lib.findType(className).fields.where((fe) => !fe.isStatic)) {
+        descriptions.add(_FieldInfo(className, fe));
       }
+
+      descriptions.sort();
+
+      descriptionMap[className] = descriptions;
     }
 
-    for (var ce in [
-      lib.findType('JsonSerializable'),
-      lib.findType('JsonKey')
-    ]) {
-      processClass(ce);
-    }
+    final buffer = StringBuffer();
 
-    final buffer = StringBuffer('# fields\n\n');
-    buffer.writeln('<dl>');
+    for (var entry in descriptionMap.entries) {
+      buffer.writeln('<h2>${entry.key}</h2>');
+      buffer.writeln('<dl>');
 
-    for (var description in descriptions.entries.toList()
-      ..sort((a, b) => a.key.compareTo(b.key))) {
-      final supportedClasses = fieldMap.entries
-          .where((e) =>
-              e.value.indexWhere(
-                  (fe) => fe.field.name == description.value.field.name) >=
-              0)
-          .map((e) => e.key)
-          .toList();
-
-      final supportedClassesHtml =
-          supportedClasses.map((c) => '<code>$c</code>').join(', ');
-
-      buffer.writeln('''
-  <dt><code>${description.key}</code></dt>
+      for (var description in entry.value) {
+        final anchor = _anchorUriForName(entry.key, description.field.name);
+        buffer.writeln('''
+  <dt><code><a name="$anchor">${description.parent}.${description.field.name}</a></code></dt>
   <dd>
-     Support classes: $supportedClassesHtml<br>
-     ${_escapeDescription(description.value.description)}
-  </dd>''');
-    }
 
-    buffer.writeln('</dl>');
+${description.description}
+  </dd>''');
+      }
+
+      buffer.writeln('</dl>');
+    }
 
     await buildStep.writeAsString(
         AssetId(buildStep.inputId.package, 'doc/doc.md'), buffer.toString());
@@ -123,4 +56,73 @@ class _DocBuilder extends Builder {
   final buildExtensions = const {
     r'lib/json_serializable.dart': ['doc/doc.md']
   };
+}
+
+const _annotationClasses = ['JsonSerializable', 'JsonKey'];
+
+String _anchorUriForName(String owner, String name) => '$owner-$name';
+
+class _FieldInfo implements Comparable<_FieldInfo> {
+  static final _ref = RegExp('\\[([^\\]]+)\\]');
+  static final _knownEntities = <String, Set<String>>{};
+
+  final String parent;
+  final FieldElement field;
+
+  String get description {
+    var description =
+        LineSplitter.split(field.documentationComment).map((line) {
+      if (line.startsWith('///')) {
+        line = line.substring(3).trimRight();
+      }
+      if (line.startsWith(' ')) {
+        // If the line is not empty, then it starts with a blank space
+        line = line.substring(1);
+      }
+      return line;
+    }).join('\n');
+
+    description = description.replaceAllMapped(_ref, (m) {
+      final ref = m[1];
+
+      String refParentClass, refName;
+      if (_knownEntities[parent].contains(ref)) {
+        refName = ref;
+        refParentClass = parent;
+      } else if (ref.contains('.')) {
+        final split = ref.split('.');
+        if (split.length == 2 &&
+            _annotationClasses.contains(split[0]) &&
+            _knownEntities[split[0]].contains(split[1])) {
+          refParentClass = split[0];
+          refName = split[1];
+        }
+      }
+
+      if (refParentClass != null) {
+        assert(refName != null);
+        return '[`$refParentClass.$refName`]'
+            '(#${_anchorUriForName(refParentClass, refName)})';
+      }
+
+      return '`$ref`';
+    });
+
+    if (parent == 'JsonSerializable') {
+      final yamlConfigKey = snakeCase(field.name);
+      description = '`build.yaml` config key: `$yamlConfigKey`\n\n$description';
+    }
+
+    return description;
+  }
+
+  _FieldInfo(this.parent, this.field) {
+    _knownEntities.putIfAbsent(parent, () => Set<String>()).add(field.name);
+  }
+
+  @override
+  int compareTo(_FieldInfo other) => field.name.compareTo(other.field.name);
+
+  @override
+  String toString() => '_FieldThing($field)';
 }
