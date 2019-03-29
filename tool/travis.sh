84912e46172b8afd0355fe71476ed1ea37b29543
Kevin Moore <kevmoo@google.com>
2019-03-29 09:44:33 -0700

    Use mono_repo v2
    Merge analyze_and_format tasks

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,70 +1,67 @@
 #!/bin/bash
-# Created with package:mono_repo v1.2.2
+# Created with package:mono_repo v2.0.0
 
-if [ -z "$PKG" ]; then
-  echo -e '\033[31mPKG environment variable must be set!\033[0m'
+if [[ -z ${PKGS} ]]; then
+  echo -e '\033[31mPKGS environment variable must be set!\033[0m'
   exit 1
 fi
 
-if [ "$#" == "0" ]; then
+if [[ "$#" == "0" ]]; then
   echo -e '\033[31mAt least one task argument must be provided!\033[0m'
   exit 1
 fi
 
-pushd $PKG
-pub upgrade || exit $?
-
 EXIT_CODE=0
 
-while (( "$#" )); do
-  TASK=$1
-  case $TASK in
-  command) echo
-    echo -e '\033[1mTASK: command\033[22m'
-    echo -e 'pub run build_runner test --delete-conflicting-outputs -- -p chrome'
-    pub run build_runner test --delete-conflicting-outputs -- -p chrome || EXIT_CODE=$?
-    ;;
-  dartanalyzer_0) echo
-    echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
-    echo -e 'dartanalyzer --fatal-infos --fatal-warnings .'
-    dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
-    ;;
-  dartanalyzer_1) echo
-    echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
-    echo -e 'dartanalyzer --fatal-warnings .'
-    dartanalyzer --fatal-warnings . || EXIT_CODE=$?
-    ;;
-  dartanalyzer_2) echo
-    echo -e '\033[1mTASK: dartanalyzer_2\033[22m'
-    echo -e 'dartanalyzer --fatal-warnings --fatal-infos .'
-    dartanalyzer --fatal-warnings --fatal-infos . || EXIT_CODE=$?
-    ;;
-  dartfmt) echo
-    echo -e '\033[1mTASK: dartfmt\033[22m'
-    echo -e 'dartfmt -n --set-exit-if-changed .'
-    dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-    ;;
-  test_0) echo
-    echo -e '\033[1mTASK: test_0\033[22m'
-    echo -e 'pub run test --run-skipped'
-    pub run test --run-skipped || EXIT_CODE=$?
-    ;;
-  test_1) echo
-    echo -e '\033[1mTASK: test_1\033[22m'
-    echo -e 'pub run test'
-    pub run test || EXIT_CODE=$?
-    ;;
-  test_2) echo
-    echo -e '\033[1mTASK: test_2\033[22m'
-    echo -e 'pub run test --run-skipped test/ensure_build_test.dart'
-    pub run test --run-skipped test/ensure_build_test.dart || EXIT_CODE=$?
-    ;;
-  *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
-    EXIT_CODE=1
-    ;;
-  esac
+for PKG in ${PKGS}; do
+  echo -e "\033[1mPKG: ${PKG}\033[22m"
+  pushd "${PKG}" || exit $?
+  pub upgrade --no-precompile || exit $?
 
-  shift
+  for TASK in "$@"; do
+    case ${TASK} in
+    command) echo
+      echo -e '\033[1mTASK: command\033[22m'
+      echo -e 'pub run build_runner test --delete-conflicting-outputs -- -p chrome'
+      pub run build_runner test --delete-conflicting-outputs -- -p chrome || EXIT_CODE=$?
+      ;;
+    dartanalyzer_0) echo
+      echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
+      echo -e 'dartanalyzer --fatal-warnings --fatal-infos .'
+      dartanalyzer --fatal-warnings --fatal-infos . || EXIT_CODE=$?
+      ;;
+    dartanalyzer_1) echo
+      echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
+      echo -e 'dartanalyzer --fatal-warnings .'
+      dartanalyzer --fatal-warnings . || EXIT_CODE=$?
+      ;;
+    dartfmt) echo
+      echo -e '\033[1mTASK: dartfmt\033[22m'
+      echo -e 'dartfmt -n --set-exit-if-changed .'
+      dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+      ;;
+    test_0) echo
+      echo -e '\033[1mTASK: test_0\033[22m'
+      echo -e 'pub run test --run-skipped'
+      pub run test --run-skipped || EXIT_CODE=$?
+      ;;
+    test_1) echo
+      echo -e '\033[1mTASK: test_1\033[22m'
+      echo -e 'pub run test'
+      pub run test || EXIT_CODE=$?
+      ;;
+    test_2) echo
+      echo -e '\033[1mTASK: test_2\033[22m'
+      echo -e 'pub run test --run-skipped test/ensure_build_test.dart'
+      pub run test --run-skipped test/ensure_build_test.dart || EXIT_CODE=$?
+      ;;
+    *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+      EXIT_CODE=1
+      ;;
+    esac
+  done
+
+  popd
 done
 
-exit $EXIT_CODE
+exit ${EXIT_CODE}
