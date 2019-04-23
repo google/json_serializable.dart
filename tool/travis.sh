19761932a45a971f76cdc2d2c76f99360af0192f
Kevin Moore <kevmoo@google.com>
2019-04-23 16:53:55 -0700

    use latest mono_repo

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with package:mono_repo v2.0.0
+# Created with package:mono_repo v2.1.0
 
 if [[ -z ${PKGS} ]]; then
   echo -e '\033[31mPKGS environment variable must be set!\033[0m'
@@ -19,43 +19,39 @@ for PKG in ${PKGS}; do
   pub upgrade --no-precompile || exit $?
 
   for TASK in "$@"; do
+    echo
+    echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
-    command) echo
-      echo -e '\033[1mTASK: command\033[22m'
-      echo -e 'pub run build_runner test --delete-conflicting-outputs -- -p chrome'
+    command)
+      echo 'pub run build_runner test --delete-conflicting-outputs -- -p chrome'
       pub run build_runner test --delete-conflicting-outputs -- -p chrome || EXIT_CODE=$?
       ;;
-    dartanalyzer_0) echo
-      echo -e '\033[1mTASK: dartanalyzer_0\033[22m'
-      echo -e 'dartanalyzer --fatal-warnings --fatal-infos .'
+    dartanalyzer_0)
+      echo 'dartanalyzer --fatal-warnings --fatal-infos .'
       dartanalyzer --fatal-warnings --fatal-infos . || EXIT_CODE=$?
       ;;
-    dartanalyzer_1) echo
-      echo -e '\033[1mTASK: dartanalyzer_1\033[22m'
-      echo -e 'dartanalyzer --fatal-warnings .'
+    dartanalyzer_1)
+      echo 'dartanalyzer --fatal-warnings .'
       dartanalyzer --fatal-warnings . || EXIT_CODE=$?
       ;;
-    dartfmt) echo
-      echo -e '\033[1mTASK: dartfmt\033[22m'
-      echo -e 'dartfmt -n --set-exit-if-changed .'
+    dartfmt)
+      echo 'dartfmt -n --set-exit-if-changed .'
       dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
       ;;
-    test_0) echo
-      echo -e '\033[1mTASK: test_0\033[22m'
-      echo -e 'pub run test --run-skipped'
+    test_0)
+      echo 'pub run test --run-skipped'
       pub run test --run-skipped || EXIT_CODE=$?
       ;;
-    test_1) echo
-      echo -e '\033[1mTASK: test_1\033[22m'
-      echo -e 'pub run test'
+    test_1)
+      echo 'pub run test'
       pub run test || EXIT_CODE=$?
       ;;
-    test_2) echo
-      echo -e '\033[1mTASK: test_2\033[22m'
-      echo -e 'pub run test --run-skipped test/ensure_build_test.dart'
+    test_2)
+      echo 'pub run test --run-skipped test/ensure_build_test.dart'
       pub run test --run-skipped test/ensure_build_test.dart || EXIT_CODE=$?
       ;;
-    *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
+    *)
+      echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
       EXIT_CODE=1
       ;;
     esac
