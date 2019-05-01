259bccda195a87e772ac9cf5170856cb86ebe3a5
Kevin Moore <kevmoo@users.noreply.github.com>
2019-05-01 08:31:14 -0700

    example_test: no points for Rube Goldberg dart SDK path (#464)

--- yaml_decode/test/example_test.dart
+++ yaml_decode/test/example_test.dart
@@ -106,18 +106,7 @@ Future<TestProcess> _run(String yamlContent) async {
   await d.file('test.yaml', yamlContent).create();
 
   return TestProcess.start(
-    dartPath,
+    Platform.resolvedExecutable,
     ['example/example.dart', _testYamlPath],
   );
 }
-
-final String dartPath = p.join(_sdkDir, 'bin', 'dart');
-
-/// The path to the root directory of the SDK.
-final String _sdkDir = (() {
-  // The Dart executable is in "/path/to/sdk/bin/dart", so two levels up is
-  // "/path/to/sdk".
-  final aboveExecutable = p.dirname(p.dirname(Platform.resolvedExecutable));
-  assert(FileSystemEntity.isFileSync(p.join(aboveExecutable, 'version')));
-  return aboveExecutable;
-})();
