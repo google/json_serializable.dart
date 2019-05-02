0815cee5ee5327cc5881a20cda3967046f5f6e83
Kevin Moore <kevmoo@google.com>
2019-05-02 12:17:40 -0700

    Dramatically simplify example_test

--- checked_yaml/test/example_test.dart
+++ checked_yaml/test/example_test.dart
@@ -2,171 +2,111 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:path/path.dart' as p;
+import 'package:checked_yaml/checked_yaml.dart';
 import 'package:test/test.dart';
-import 'package:test_descriptor/test_descriptor.dart' as d;
-import 'package:test_process/test_process.dart';
+
+import '../example/example.dart' as example;
 
 void main() {
-  test('valid input', () async {
-    final proc = await _run('{"name": "bob", "count": 42}');
-
-    await expectLater(
-      proc.stdout,
-      emitsInOrder(['Configuration: {name: bob, count: 42}']),
+  test('valid input', () {
+    expect(
+      () => _run('{"name": "bob", "count": 42}'),
+      prints('''
+Configuration: {name: bob, count: 42}
+'''),
     );
-
-    await proc.shouldExit(0);
   });
 
-  test('empty map', () async {
-    final proc = await _run('{}');
-
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 1 of $_testYamlPath: Required keys are missing: name, count.
+  test('empty map', () {
+    _expectThrows(
+      '{}',
+      r'''
+line 1, column 1: Required keys are missing: name, count.
   ╷
 1 │ {}
   │ ^^
-  ╵
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
 
-  test('not a map', () async {
-    final proc = await _run('42');
-
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 1 of $_testYamlPath: Not a map
+  test('not a map', () {
+    _expectThrows(
+      '42',
+      r'''
+line 1, column 1: Not a map
   ╷
 1 │ 42
   │ ^^
-  ╵
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
 
-  test('invalid yaml', () async {
-    final proc = await _run('{');
-
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 2 of $_testYamlPath: Expected node content.
+  test('invalid yaml', () {
+    _expectThrows(
+      '{',
+      r'''
+line 1, column 2: Expected node content.
   ╷
 1 │ {
   │  ^
-  ╵
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
 
-  test('duplicate keys', () async {
-    final proc = await _run('{"a":null, "a":null}');
-
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 12 of $_testYamlPath: Duplicate mapping key.
+  test('duplicate keys', () {
+    _expectThrows(
+      '{"a":null, "a":null}',
+      r'''
+line 1, column 12: Duplicate mapping key.
   ╷
 1 │ {"a":null, "a":null}
   │            ^^^
-  ╵
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
 
-  test('unexpected key', () async {
-    final proc = await _run('{"bob": 42}');
-
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 2 of $_testYamlPath: Unrecognized keys: [bob]; supported keys: [name, count]
+  test('unexpected key', () {
+    _expectThrows(
+      '{"bob": 42}',
+      r'''
+line 1, column 2: Unrecognized keys: [bob]; supported keys: [name, count]
   ╷
 1 │ {"bob": 42}
   │  ^^^^^
-  ╵
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
 
-  test('bad name type', () async {
-    final proc = await _run('{"name": 42, "count": 42}');
-
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 10 of $_testYamlPath: Unsupported value for "name".
+  test('bad name type', () {
+    _expectThrows(
+      '{"name": 42, "count": 42}',
+      r'''
+line 1, column 10: Unsupported value for "name".
   ╷
 1 │ {"name": 42, "count": 42}
   │          ^^
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
-  test('bad name contents', () async {
-    final proc = await _run('{"name": "", "count": 42}');
 
-    await expectLater(
-      proc.stderr,
-      emitsInOrder(
-        LineSplitter.split('''
-Unhandled exception:
-ParsedYamlException: line 1, column 10 of $_testYamlPath: Unsupported value for "name". Cannot be empty.
+  test('bad name contents', () {
+    _expectThrows(
+      '{"name": "", "count": 42}',
+      r'''
+line 1, column 10: Unsupported value for "name". Cannot be empty.
   ╷
 1 │ {"name": "", "count": 42}
   │          ^^
-'''),
-      ),
+  ╵''',
     );
-
-    await proc.shouldExit(isNot(0));
   });
 }
 
-String get _testYamlPath => p.join(d.sandbox, 'test.yaml');
+void _expectThrows(String yamlContent, matcher) => expect(
+    () => _run(yamlContent),
+    throwsA(isA<ParsedYamlException>().having((e) {
+      printOnFailure("r'''\n${e.formattedMessage}'''");
+      return e.formattedMessage;
+    }, 'formattedMessage', matcher)));
 
-Future<TestProcess> _run(String yamlContent) async {
-  await d.file('test.yaml', yamlContent).create();
-
-  return TestProcess.start(
-    Platform.resolvedExecutable,
-    ['example/example.dart', _testYamlPath],
-  );
-}
+void _run(String yamlContent) => example.main([yamlContent]);
