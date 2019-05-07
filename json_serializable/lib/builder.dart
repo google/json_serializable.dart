fe93fdb0abc2c02153179d2797dc0ac17e1ce257
Kevin Moore <kevmoo@google.com>
2019-05-06 23:15:29 -0700

    Update links for pub.dev

--- json_serializable/lib/builder.dart
+++ json_serializable/lib/builder.dart
@@ -5,7 +5,7 @@
 /// Configuration for using `package:build`-compatible build systems.
 ///
 /// See:
-/// * [build_runner](https://pub.dartlang.org/packages/build_runner)
+/// * [build_runner](https://pub.dev/packages/build_runner)
 ///
 /// This library is **not** intended to be imported by typical end-users unless
 /// you are creating a custom compilation pipeline. See documentation for
