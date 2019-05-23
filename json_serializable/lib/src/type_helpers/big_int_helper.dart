2dceb1bdb22dd6ee9f57879545d7ae983f1ff0e2
Kevin Moore <kevmoo@google.com>
2019-05-22 18:41:34 -0700

    DRY up implementation of TypeHelper impls that map to String

--- json_serializable/lib/src/type_helpers/big_int_helper.dart
+++ json_serializable/lib/src/type_helpers/big_int_helper.dart
@@ -3,44 +3,20 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/dart/element/type.dart';
-import 'package:source_gen/source_gen.dart' show TypeChecker;
+
 import '../type_helper.dart';
+import 'to_from_string.dart';
 
 class BigIntHelper extends TypeHelper {
   const BigIntHelper();
 
   @override
   String serialize(
-      DartType targetType, String expression, TypeHelperContext context) {
-    if (!_matchesType(targetType)) {
-      return null;
-    }
-
-    final buffer = StringBuffer(expression);
-
-    if (context.nullable) {
-      buffer.write('?');
-    }
-
-    buffer.write('.toString()');
-
-    return buffer.toString();
-  }
+          DartType targetType, String expression, TypeHelperContext context) =>
+      bigIntString.serialize(targetType, expression, context.nullable);
 
   @override
   String deserialize(
-      DartType targetType, String expression, TypeHelperContext context) {
-    if (!_matchesType(targetType)) {
-      return null;
-    }
-
-    return commonNullPrefix(
-      context.nullable,
-      expression,
-      'BigInt.parse($expression as String)',
-    ).toString();
-  }
+          DartType targetType, String expression, TypeHelperContext context) =>
+      bigIntString.deserialize(targetType, expression, context.nullable, false);
 }
-
-bool _matchesType(DartType type) =>
-    const TypeChecker.fromUrl('dart:core#BigInt').isExactlyType(type);
