70c4bcebf1e11c28fdcc1b953021a251d1a6f59a
Kevin Moore <kevmoo@google.com>
2019-07-19 21:09:04 -0700

    rebuild sources

--- example/lib/json_converter_example.g.dart
+++ example/lib/json_converter_example.g.dart
@@ -8,11 +8,11 @@ part of 'json_converter_example.dart';
 
 GenericCollection<T> _$GenericCollectionFromJson<T>(Map<String, dynamic> json) {
   return GenericCollection<T>(
-      page: json['page'] as int,
-      totalResults: json['total_results'] as int,
-      totalPages: json['total_pages'] as int,
-      results:
-          (json['results'] as List)?.map(_Converter<T>().fromJson)?.toList());
+    page: json['page'] as int,
+    totalResults: json['total_results'] as int,
+    totalPages: json['total_pages'] as int,
+    results: (json['results'] as List)?.map(_Converter<T>().fromJson)?.toList(),
+  );
 }
 
 Map<String, dynamic> _$GenericCollectionToJson<T>(
@@ -21,12 +21,18 @@ Map<String, dynamic> _$GenericCollectionToJson<T>(
       'page': instance.page,
       'total_results': instance.totalResults,
       'total_pages': instance.totalPages,
-      'results': instance.results?.map(_Converter<T>().toJson)?.toList()
+      'results': instance.results?.map(_Converter<T>().toJson)?.toList(),
     };
 
 CustomResult _$CustomResultFromJson(Map<String, dynamic> json) {
-  return CustomResult(json['name'] as String, json['size'] as int);
+  return CustomResult(
+    json['name'] as String,
+    json['size'] as int,
+  );
 }
 
 Map<String, dynamic> _$CustomResultToJson(CustomResult instance) =>
-    <String, dynamic>{'name': instance.name, 'size': instance.size};
+    <String, dynamic>{
+      'name': instance.name,
+      'size': instance.size,
+    };
