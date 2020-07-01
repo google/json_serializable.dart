29fa2b7f18cfe63bb852cdf3cce56015d80dd5e5
Kevin Moore <kevmoo@google.com>
2020-07-01 15:27:29 -0700

    tiny bit enabled

--- json_serializable/example/example.dart
+++ json_serializable/example/example.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart=2.9
+
 import 'package:json_annotation/json_annotation.dart';
 
 part 'example.g.dart';
@@ -10,8 +12,8 @@ part 'example.g.dart';
 class Person {
   final String firstName;
   final String lastName;
-  final DateTime dateOfBirth;
-  Person({this.firstName, this.lastName, this.dateOfBirth});
+  final DateTime? dateOfBirth;
+  Person({required this.firstName, required this.lastName, this.dateOfBirth});
   factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
   Map<String, dynamic> toJson() => _$PersonToJson(this);
 }
