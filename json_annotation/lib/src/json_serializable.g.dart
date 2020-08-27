e8a5dbbf07c49f73355359ebb9a69daab1b6d3b1
Kevin Moore <kevmoo@google.com>
2020-08-26 17:01:35 -0700

    Regenerate JsonSerializable

--- json_annotation/lib/src/json_serializable.g.dart
+++ json_annotation/lib/src/json_serializable.g.dart
@@ -18,7 +18,7 @@ JsonSerializable _$JsonSerializableFromJson(Map<String, dynamic> json) {
       'field_rename',
       'ignore_unannotated',
       'include_if_null',
-      'nullable',
+      'nullable'
     ]);
     final val = JsonSerializable(
       anyMap: $checkedConvert(json, 'any_map', (v) => v as bool),
@@ -45,7 +45,7 @@ JsonSerializable _$JsonSerializableFromJson(Map<String, dynamic> json) {
     'explicitToJson': 'explicit_to_json',
     'fieldRename': 'field_rename',
     'ignoreUnannotated': 'ignore_unannotated',
-    'includeIfNull': 'include_if_null',
+    'includeIfNull': 'include_if_null'
   });
 }
 
@@ -63,29 +63,41 @@ Map<String, dynamic> _$JsonSerializableToJson(JsonSerializable instance) =>
       'nullable': instance.nullable,
     };
 
-T _$enumDecode<T>(Map<T, dynamic> enumValues, dynamic source) {
+T _$enumDecode<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
   if (source == null) {
     throw ArgumentError('A value must be provided. Supported values: '
         '${enumValues.values.join(', ')}');
   }
-  return enumValues.entries
-      .singleWhere((e) => e.value == source,
-          orElse: () => throw ArgumentError(
-              '`$source` is not one of the supported values: '
-              '${enumValues.values.join(', ')}'))
-      .key;
+
+  final value = enumValues.entries
+      .singleWhere((e) => e.value == source, orElse: () => null)
+      ?.key;
+
+  if (value == null && unknownValue == null) {
+    throw ArgumentError('`$source` is not one of the supported values: '
+        '${enumValues.values.join(', ')}');
+  }
+  return value ?? unknownValue;
 }
 
-T _$enumDecodeNullable<T>(Map<T, dynamic> enumValues, dynamic source) {
+T _$enumDecodeNullable<T>(
+  Map<T, dynamic> enumValues,
+  dynamic source, {
+  T unknownValue,
+}) {
   if (source == null) {
     return null;
   }
-  return _$enumDecode<T>(enumValues, source);
+  return _$enumDecode<T>(enumValues, source, unknownValue: unknownValue);
 }
 
-const _$FieldRenameEnumMap = <FieldRename, dynamic>{
+const _$FieldRenameEnumMap = {
   FieldRename.none: 'none',
   FieldRename.kebab: 'kebab',
   FieldRename.snake: 'snake',
-  FieldRename.pascal: 'pascal'
+  FieldRename.pascal: 'pascal',
 };
