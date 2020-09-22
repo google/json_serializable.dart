243e9b2de15743f621ab657297aa2dccc0cb2af9
Kevin Moore <kevmoo@google.com>
2020-09-21 17:45:59 -0700

    Move generator helper into its own file

--- json_serializable/lib/src/json_serializable_generator.dart
+++ json_serializable/lib/src/json_serializable_generator.dart
@@ -7,10 +7,7 @@ import 'package:build/build.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'decode_helper.dart';
-import 'encoder_helper.dart';
-import 'field_helpers.dart';
-import 'helper_core.dart';
+import 'generator_helper.dart';
 import 'settings.dart';
 import 'type_helper.dart';
 import 'type_helpers/big_int_helper.dart';
@@ -18,7 +15,6 @@ import 'type_helpers/date_time_helper.dart';
 import 'type_helpers/duration_helper.dart';
 import 'type_helpers/json_helper.dart';
 import 'type_helpers/uri_helper.dart';
-import 'utils.dart';
 
 class JsonSerializableGenerator
     extends GeneratorForAnnotation<JsonSerializable> {
@@ -73,109 +69,7 @@ class JsonSerializableGenerator
     }
 
     final classElement = element as ClassElement;
-    final helper = _GeneratorHelper(_settings, classElement, annotation);
-    return helper._generate();
-  }
-}
-
-class _GeneratorHelper extends HelperCore with EncodeHelper, DecodeHelper {
-  final Settings _generator;
-  final _addedMembers = <String>{};
-
-  _GeneratorHelper(
-    this._generator,
-    ClassElement element,
-    ConstantReader annotation,
-  ) : super(
-            element,
-            mergeConfig(
-              _generator.config,
-              annotation,
-              classElement: element,
-            ));
-
-  @override
-  void addMember(String memberContent) {
-    _addedMembers.add(memberContent);
-  }
-
-  @override
-  Iterable<TypeHelper> get allTypeHelpers => _generator.allHelpers;
-
-  Iterable<String> _generate() sync* {
-    assert(_addedMembers.isEmpty);
-
-    if (config.genericArgumentFactories && element.typeParameters.isEmpty) {
-      log.warning(
-        'The class `${element.displayName}` is annotated '
-        'with `JsonSerializable` field `genericArgumentFactories: true`. '
-        '`genericArgumentFactories: true` only affects classes with type '
-        'parameters. For classes without type parameters, the option is '
-        'ignored.',
-      );
-    }
-
-    final sortedFields = createSortedFieldSet(element);
-
-    // Used to keep track of why a field is ignored. Useful for providing
-    // helpful errors when generating constructor calls that try to use one of
-    // these fields.
-    final unavailableReasons = <String, String>{};
-
-    final accessibleFields = sortedFields.fold<Map<String, FieldElement>>(
-      <String, FieldElement>{},
-      (map, field) {
-        if (!field.isPublic) {
-          unavailableReasons[field.name] = 'It is assigned to a private field.';
-        } else if (field.getter == null) {
-          assert(field.setter != null);
-          unavailableReasons[field.name] =
-              'Setter-only properties are not supported.';
-          log.warning('Setters are ignored: ${element.name}.${field.name}');
-        } else if (jsonKeyFor(field).ignore) {
-          unavailableReasons[field.name] =
-              'It is assigned to an ignored field.';
-        } else {
-          assert(!map.containsKey(field.name));
-          map[field.name] = field;
-        }
-
-        return map;
-      },
-    );
-
-    var accessibleFieldSet = accessibleFields.values.toSet();
-    if (config.createFactory) {
-      final createResult = createFactory(accessibleFields, unavailableReasons);
-      yield createResult.output;
-
-      accessibleFieldSet = accessibleFields.entries
-          .where((e) => createResult.usedFields.contains(e.key))
-          .map((e) => e.value)
-          .toSet();
-    }
-
-    // Check for duplicate JSON keys due to colliding annotations.
-    // We do this now, since we have a final field list after any pruning done
-    // by `_writeCtor`.
-    accessibleFieldSet.fold(
-      <String>{},
-      (Set<String> set, fe) {
-        final jsonKey = nameAccess(fe);
-        if (!set.add(jsonKey)) {
-          throw InvalidGenerationSourceError(
-            'More than one field has the JSON key for name "$jsonKey".',
-            element: fe,
-          );
-        }
-        return set;
-      },
-    );
-
-    if (config.createToJson) {
-      yield* createToJson(accessibleFieldSet);
-    }
-
-    yield* _addedMembers;
+    final helper = GeneratorHelper(_settings, classElement, annotation);
+    return helper.generate();
   }
 }
