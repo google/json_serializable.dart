af3c57d28af8967560e11f1353253b5112720dbb
Kevin Moore <kevmoo@google.com>
2021-02-24 20:51:03 -0800

    Remove experiment bits

--- json_serializable/test/json_serializable_test.dart
+++ json_serializable/test/json_serializable_test.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 @TestOn('vm')
-import 'package:build/experiments.dart';
 import 'package:json_serializable/json_serializable.dart';
 import 'package:path/path.dart' as p;
 import 'package:source_gen_test/source_gen_test.dart';
@@ -11,12 +10,9 @@ import 'package:test/test.dart';
 
 Future<void> main() async {
   initializeBuildLogTracking();
-  final reader = await withEnabledExperiments(
-    () => initializeLibraryReaderForDirectory(
-      p.join('test', 'src'),
-      '_json_serializable_test_input.dart',
-    ),
-    ['non-nullable'],
+  final reader = await initializeLibraryReaderForDirectory(
+    p.join('test', 'src'),
+    '_json_serializable_test_input.dart',
   );
 
   testAnnotatedElements(
