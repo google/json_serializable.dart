8e6b2fadd62de6fde6950c5b144e769687112c09
Guy Kogus <guy.kogus@ridewithvia.com>
2021-08-16 18:16:00 +0200

    Remove unused code

--- json_annotation/lib/src/json_serializable.g.dart
+++ json_annotation/lib/src/json_serializable.g.dart
@@ -72,32 +72,6 @@ Map<String, dynamic> _$JsonSerializableToJson(JsonSerializable instance) =>
       'include_if_null': instance.includeIfNull,
     };
 
-K _$enumDecode<K, V>(
-  Map<K, V> enumValues,
-  Object? source, {
-  K? unknownValue,
-}) {
-  if (source == null) {
-    throw ArgumentError(
-      'A value must be provided. Supported values: '
-      '${enumValues.values.join(', ')}',
-    );
-  }
-
-  return enumValues.entries.singleWhere(
-    (e) => e.value == source,
-    orElse: () {
-      if (unknownValue == null) {
-        throw ArgumentError(
-          '`$source` is not one of the supported values: '
-          '${enumValues.values.join(', ')}',
-        );
-      }
-      return MapEntry(unknownValue, enumValues.values.first);
-    },
-  ).key;
-}
-
 K? _$enumDecodeNullable<K, V>(
   Map<K, V> enumValues,
   dynamic source, {
