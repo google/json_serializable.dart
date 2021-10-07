4db27a8d0e3b48fd4a9a872f008adde77eb9ae30
Kevin Moore <kevmoo@users.noreply.github.com>
2021-10-07 11:14:09 -0700

    Big documentation updates/improvements (#1003)

--- json_serializable/example/example.dart
+++ json_serializable/example/example.dart
@@ -8,10 +8,19 @@ part 'example.g.dart';
 
 @JsonSerializable()
 class Person {
-  final String firstName;
-  final String lastName;
+  /// The generated code assumes these values exist in JSON.
+  final String firstName, lastName;
+
+  /// The generated code below handles if the corresponding JSON value doesn't
+  /// exist or is empty.
   final DateTime? dateOfBirth;
+
   Person({required this.firstName, required this.lastName, this.dateOfBirth});
+
+  /// Connect the generated [_$PersonFromJson] function to the `fromJson`
+  /// factory.
   factory Person.fromJson(Map<String, dynamic> json) => _$PersonFromJson(json);
+
+  /// Connect the generated [_$PersonToJson] function to the `toJson` method.
   Map<String, dynamic> toJson() => _$PersonToJson(this);
 }
