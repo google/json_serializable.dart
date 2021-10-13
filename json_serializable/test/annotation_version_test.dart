4781b7ece290ee2f808d1a4375f13ed0d65a97f6
Kevin Moore <kevmoo@google.com>
2021-10-13 14:03:17 -0700

    cleanup

--- json_serializable/test/annotation_version_test.dart
+++ json_serializable/test/annotation_version_test.dart
@@ -13,17 +13,16 @@ import 'package:test_process/test_process.dart';
 
 const _annotationLowerBound = '4.2.0';
 
-const _missingProductionDep = '''
-[SEVERE] json_serializable:json_serializable on lib/sample.dart:
-
-You are missing a required dependency on json_annotation in the "dependencies" section of your pubspec with a lower bound of at least "$_annotationLowerBound".''';
+const _missingProductionDep =
+    'You are missing a required dependency on json_annotation in the '
+    '"dependencies" section of your pubspec with a lower bound of at least '
+    '"$_annotationLowerBound".';
 
 void main() {
   test(
     'missing dependency in production code',
     () => _structurePackage(
       sourceDirectory: 'lib',
-      exitCode: 1,
       message: _missingProductionDep,
     ),
   );
@@ -32,11 +31,9 @@ void main() {
     'missing dependency in example code',
     () => _structurePackage(
       sourceDirectory: 'example',
-      exitCode: 1,
-      message: '''
-[SEVERE] json_serializable:json_serializable on example/sample.dart:
-
-You are missing a required dependency on json_annotation with a lower bound of at least "$_annotationLowerBound".''',
+      message:
+          'You are missing a required dependency on json_annotation with a '
+          'lower bound of at least "$_annotationLowerBound".',
     ),
   );
 
@@ -44,7 +41,6 @@ You are missing a required dependency on json_annotation with a lower bound of a
     'dev dependency with a production usage',
     () => _structurePackage(
       sourceDirectory: 'lib',
-      exitCode: 1,
       devDependencies: {'json_annotation': _annotationLowerBound},
       message: _missingProductionDep,
     ),
@@ -54,12 +50,10 @@ You are missing a required dependency on json_annotation with a lower bound of a
     'dependency with `null` constraint',
     () => _structurePackage(
       sourceDirectory: 'lib',
-      exitCode: 1,
       dependencies: {'json_annotation': null},
-      message: '''
-[SEVERE] json_serializable:json_serializable on lib/sample.dart:
-
-The version constraint "any" on json_annotation allows versions before $_annotationLowerBound which is not allowed.''',
+      message:
+          'The version constraint "any" on json_annotation allows versions '
+          'before $_annotationLowerBound which is not allowed.',
     ),
   );
 
@@ -67,12 +61,10 @@ The version constraint "any" on json_annotation allows versions before $_annotat
     'dependency with "any" constraint',
     () => _structurePackage(
       sourceDirectory: 'lib',
-      exitCode: 1,
       dependencies: {'json_annotation': 'any'},
-      message: '''
-[SEVERE] json_serializable:json_serializable on lib/sample.dart:
-
-The version constraint "any" on json_annotation allows versions before $_annotationLowerBound which is not allowed.''',
+      message:
+          'The version constraint "any" on json_annotation allows versions '
+          'before $_annotationLowerBound which is not allowed.',
     ),
   );
 
@@ -80,19 +72,16 @@ The version constraint "any" on json_annotation allows versions before $_annotat
     'dependency with too low version range',
     () => _structurePackage(
       sourceDirectory: 'lib',
-      exitCode: 1,
       dependencies: {'json_annotation': '^4.0.0'},
-      message: '''
-[SEVERE] json_serializable:json_serializable on lib/sample.dart:
-
-The version constraint "^4.0.0" on json_annotation allows versions before $_annotationLowerBound which is not allowed.''',
+      message:
+          'The version constraint "^4.0.0" on json_annotation allows versions '
+          'before $_annotationLowerBound which is not allowed.',
     ),
   );
 }
 
 Future<void> _structurePackage({
   required String sourceDirectory,
-  required int exitCode,
   required String message,
   Map<String, dynamic> dependencies = const {},
   Map<String, dynamic> devDependencies = const {},
@@ -140,11 +129,15 @@ class SomeClass{}
   await expectLater(
     proc.stdout,
     emitsThrough(
-      emitsInOrder(LineSplitter.split(message)),
+      emitsInOrder([
+        '[SEVERE] json_serializable:json_serializable on $sourceDirectory/sample.dart:',
+        '',
+        message,
+      ]),
     ),
   );
 
   await expectLater(proc.stderr, emitsDone);
 
-  await proc.shouldExit(exitCode);
+  await proc.shouldExit(1);
 }
