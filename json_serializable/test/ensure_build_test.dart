3fccfc3901a86b8f160851e6e4b1512a37fbf2e8
Kevin Moore <kevmoo@google.com>
2021-12-10 13:28:19 -0800

    latest build_verify

--- json_serializable/test/ensure_build_test.dart
+++ json_serializable/test/ensure_build_test.dart
@@ -8,6 +8,9 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build',
-      () => expectBuildClean(packageRelativeDirectory: 'json_serializable'));
+  test(
+    'ensure_build',
+    () => expectBuildClean(packageRelativeDirectory: 'json_serializable'),
+    timeout: const Timeout.factor(2),
+  );
 }
