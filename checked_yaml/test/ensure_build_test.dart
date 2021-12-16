90d3e629ecb05d791b4fb928068501a981bddd28
Kevin Moore <kevmoo@google.com>
2021-12-16 11:30:17 -0800

    Add 2x timeout factor to all "ensure_build" tests

--- checked_yaml/test/ensure_build_test.dart
+++ checked_yaml/test/ensure_build_test.dart
@@ -8,6 +8,9 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build',
-      () => expectBuildClean(packageRelativeDirectory: 'checked_yaml'));
+  test(
+    'ensure_build',
+    () => expectBuildClean(packageRelativeDirectory: 'checked_yaml'),
+    timeout: const Timeout.factor(2),
+  );
 }
--- example/test/ensure_build_test.dart
+++ example/test/ensure_build_test.dart
@@ -7,6 +7,9 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build',
-      () => expectBuildClean(packageRelativeDirectory: 'example'));
+  test(
+    'ensure_build',
+    () => expectBuildClean(packageRelativeDirectory: 'example'),
+    timeout: const Timeout.factor(2),
+  );
 }
