d3c1693a24718d8b9cd04858abde904c9f721eb9
evanweible-wf <evan.weible@workiva.com>
2021-12-20 17:05:25 -0700

    mono_repo 5.0.0 and changes to fix CI failures
    - Run on 2.13.4 instead of 2.7. This is necessary because of some of the
    newer dependencies that the widened analyzer range pulls in.
    - Run on stable instead of dev. This change was made because building on
    dev at this point is broken due to outdated dependencies. Building on
    stable, however, does work.
    - Run formatter check on 2.13.4 instead of dev to avoid a formatting
    discrepancy between the builder output (which uses dart_style) and the
    `dart format` output on dev.

--- json_serializable/lib/src/encoder_helper.dart
+++ json_serializable/lib/src/encoder_helper.dart
@@ -120,7 +120,9 @@ abstract class EncodeHelper implements HelperCore {
       }
     }
 
-    buffer..writeln('    return $generatedLocalVarName;')..writeln('  }');
+    buffer
+      ..writeln('    return $generatedLocalVarName;')
+      ..writeln('  }');
   }
 
   String _serializeField(FieldElement field, String accessExpression) {
