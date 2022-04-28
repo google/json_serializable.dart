2dd62e52abdb8e36c18c308b858cfbf8438b1967
Kevin Moore <kevmoo@google.com>
2022-04-28 10:35:03 -0700

    update

--- tool/ci.sh
+++ tool/ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with package:mono_repo v6.2.0
+# Created with package:mono_repo v6.2.2
 
 # Support built in commands on windows out of the box.
 # When it is a flutter repo (check the pubspec.yaml for "sdk: flutter")
