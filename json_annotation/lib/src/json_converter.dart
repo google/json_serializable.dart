3799e5869d64fc17a0d4aa988e4bc94b35f52d5b
Remi Rousselet <darky12s@gmail.com>
2022-05-09 11:46:07 +0200

    Add changelog entry and bump version

--- json_annotation/lib/src/json_converter.dart
+++ json_annotation/lib/src/json_converter.dart
@@ -8,6 +8,37 @@
 ///
 /// [S] is the type of the value stored in JSON. It must be a valid JSON type
 /// such as [String], [int], or [Map<String, dynamic>].
+///
+///
+/// [JsonConverter]s can be placed either on the class:
+///
+/// ```dart
+/// class MyConverter extends JsonConverter<Value, JSON> {
+///   // TODO
+/// }
+///
+/// @JsonSerializable()
+/// @MyJsonConverter()
+/// class Example {}
+/// ```
+///
+/// or on a property:
+///
+/// ```dart
+/// @JsonSerializable()
+/// @MyJsonConverter()
+/// class Example {
+///   @MyJsonConverter()
+///   final Value property;
+/// }
+/// ```
+///
+/// Or finally, passed to the annotation:
+///
+///```dart
+/// @JsonSerializable(converters: [MyConverter()])
+/// class Example {}
+/// ```
 abstract class JsonConverter<T, S> {
   const JsonConverter();
 
