dcdb496be7c5911eb20190e56fa7201bc016386b
Kevin Moore <kevmoo@google.com>
2023-01-13 15:05:17 -0800

    Latest mono_repo
    Closes https://github.com/google/json_serializable.dart/pull/1263
    Closes https://github.com/google/json_serializable.dart/pull/1264

--- tool/ci.sh
+++ tool/ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Created with package:mono_repo v6.4.2
+# Created with package:mono_repo v6.4.3
 
 # Support built in commands on windows out of the box.
 # When it is a flutter repo (check the pubspec.yaml for "sdk: flutter")
