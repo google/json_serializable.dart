d30c2fdb0598d548c25705931531842957623fdd
Kevin Moore <kevmoo@users.noreply.github.com>
2025-06-04 19:40:16 -0500

    checked_yaml: prepare release (#1500)
    Did some cleanup of the implementation so the control flow reads easier
    Added a missing test to handle the `null` case.

--- checked_yaml/test/custom_error_test.dart
+++ checked_yaml/test/custom_error_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 import 'package:yaml/yaml.dart';
 
 void main() {
-  test('bob', () {
+  test('simple test', () {
     expect(
       () => checkedYamlDecode('{"innerMap": {}}', (m) {
         throw CheckedFromJsonException(
@@ -32,6 +32,37 @@ line 1, column 14: There was an error parsing the map.
   ╷
 1 │ {"innerMap": {}}
   │              ^^
+  ╵'''),
+      ),
+    );
+  });
+
+  test('null map - allowed', () {
+    expect(
+      () => checkedYamlDecode(allowNull: true, 'null', (m) => Object()),
+      isA<Object>(),
+    );
+  });
+
+  test('null map - not allowed', () {
+    expect(
+      () => checkedYamlDecode('null', (m) {
+        throw TestFailure('should never get here!');
+      }),
+      throwsA(
+        isA<ParsedYamlException>()
+            .having((e) => e.message, 'message', 'Not a map')
+            .having(
+              (e) => e.yamlNode,
+              'yamlNode',
+              isA<YamlScalar>().having((s) => s.value, 'value', isNull),
+            )
+            .having((e) => e.innerError, 'innerError', isNull)
+            .having((e) => e.formattedMessage, 'formattedMessage', '''
+line 1, column 1: Not a map
+  ╷
+1 │ null
+  │ ^^^^
   ╵'''),
       ),
     );
