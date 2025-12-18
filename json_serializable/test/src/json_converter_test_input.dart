350961cad3998efffdf2d3451fe2a11283acf744
taeilyoon <zkrxpdlf0808@gmail.com>
2025-12-19 00:35:59 +0900

    feat: Allow JsonConverter annotations to use constructor arguments and add tests for various argument types.

--- json_serializable/test/src/json_converter_test_input.dart
+++ json_serializable/test/src/json_converter_test_input.dart
@@ -152,10 +152,20 @@ class _DurationMillisecondConverter implements JsonConverter<Duration, int> {
   int toJson(Duration object) => throw UnimplementedError();
 }
 
-@ShouldThrow(
-  'Generators with constructor arguments are not supported.',
-  element: 'new',
-)
+@ShouldGenerate(r'''
+JsonConverterCtorParams _$JsonConverterCtorParamsFromJson(
+  Map<String, dynamic> json,
+) => JsonConverterCtorParams()
+  ..value = const _ConverterWithCtorParams(
+    42,
+  ).fromJson((json['value'] as num).toInt());
+
+Map<String, dynamic> _$JsonConverterCtorParamsToJson(
+  JsonConverterCtorParams instance,
+) => <String, dynamic>{
+  'value': const _ConverterWithCtorParams(42).toJson(instance.value),
+};
+''')
 @JsonSerializable()
 @_ConverterWithCtorParams(42)
 class JsonConverterCtorParams {
@@ -220,4 +230,100 @@ class _NullableConverter implements JsonConverter<NeedsConversion?, Object?> {
 
   @override
   Object? toJson(NeedsConversion? object) => null;
+}
+
+@ShouldGenerate(r'''
+JsonConverterStringParams _$JsonConverterStringParamsFromJson(
+  Map<String, dynamic> json,
+) => JsonConverterStringParams()
+  ..value = const _ConverterWithStringParams(
+    'cool',
+  ).fromJson((json['value'] as num).toInt());
+
+Map<String, dynamic> _$JsonConverterStringParamsToJson(
+  JsonConverterStringParams instance,
+) => <String, dynamic>{
+  'value': const _ConverterWithStringParams('cool').toJson(instance.value),
+};
+''')
+@JsonSerializable()
+@_ConverterWithStringParams('cool')
+class JsonConverterStringParams {
+  late Duration value;
+}
+
+class _ConverterWithStringParams implements JsonConverter<Duration, int> {
+  final String param;
+
+  const _ConverterWithStringParams(this.param);
+
+  @override
+  Duration fromJson(int json) => throw UnimplementedError();
+
+  @override
+  int toJson(Duration object) => 0;
+}
+
+@ShouldGenerate(r'''
+JsonConverterListParams _$JsonConverterListParamsFromJson(
+  Map<String, dynamic> json,
+) => JsonConverterListParams()
+  ..value = const _ConverterWithListParams([
+    42,
+  ]).fromJson((json['value'] as num).toInt());
+
+Map<String, dynamic> _$JsonConverterListParamsToJson(
+  JsonConverterListParams instance,
+) => <String, dynamic>{
+  'value': const _ConverterWithListParams([42]).toJson(instance.value),
+};
+''')
+@JsonSerializable()
+@_ConverterWithListParams([42])
+class JsonConverterListParams {
+  late Duration value;
+}
+
+class _ConverterWithListParams implements JsonConverter<Duration, int> {
+  final List<int> param;
+
+  const _ConverterWithListParams(this.param);
+
+  @override
+  Duration fromJson(int json) => throw UnimplementedError();
+
+  @override
+  int toJson(Duration object) => 0;
+}
+
+@ShouldGenerate(r'''
+JsonConverterNamedParams _$JsonConverterNamedParamsFromJson(
+  Map<String, dynamic> json,
+) => JsonConverterNamedParams()
+  ..value = const _ConverterWithNamedParams(
+    param: 42,
+  ).fromJson((json['value'] as num).toInt());
+
+Map<String, dynamic> _$JsonConverterNamedParamsToJson(
+  JsonConverterNamedParams instance,
+) => <String, dynamic>{
+  'value': const _ConverterWithNamedParams(param: 42).toJson(instance.value),
+};
+''')
+@JsonSerializable()
+@_ConverterWithNamedParams(param: 42)
+class JsonConverterNamedParams {
+  late Duration value;
+}
+
+class _ConverterWithNamedParams implements JsonConverter<Duration, int> {
+  final int param;
+
+  const _ConverterWithNamedParams({required this.param});
+
+  @override
+  Duration fromJson(int json) => throw UnimplementedError();
+
+  @override
+  int toJson(Duration object) => 0;
 }
